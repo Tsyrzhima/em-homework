2851bab0957e021977ee0cacddd6b22fd3986d90
Tsyrzhima <sss1992bur@mail.ru>
2025-05-28 14:55:47 +0800

    Решение задачи 4: использование Enums в PHP 8.1

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+// Задание 1: Использование match (PHP 8.0)
 function getStatusMessage(string $status): string
 {
     return match ($status) {
@@ -14,6 +15,7 @@ echo getStatusMessage('error') . PHP_EOL;
 echo getStatusMessage('pending') . PHP_EOL;
 echo getStatusMessage('unknown') . PHP_EOL;
 
+// Задание 2: Использование named arguments (PHP 8.0)
 function calculatePrice(int|float $basePrice, int|float $discount, int|float $tax): float
 {
     $priceAfterDiscount = $basePrice - ($basePrice * $discount / 100);
@@ -25,6 +27,7 @@ function calculatePrice(int|float $basePrice, int|float $discount, int|float $ta
 echo calculatePrice(basePrice: 1000, discount: 10, tax: 5) . PHP_EOL;
 echo calculatePrice(tax: 5, discount: 10, basePrice: 2000) . PHP_EOL;
 
+// Задание 3: readonly свойства в классах (PHP 8.1)
 class User
 {
     public readonly int $id;
@@ -41,4 +44,19 @@ class User
 
 $user = new User(1, 'Иван', 'ivan@example.com');
 echo $user->name . PHP_EOL;
-$user->name = 'Петр';
+//$user->name = 'Петр';
+
+// Задание 4: Enums (PHP 8.1)
+enum OrderStatus: string{
+    case Pending = "Заказ в ожидании";
+    case Shipped = "Заказ отправлен";
+    case Delivered = "Заказ доставлен";
+}
+function getDeliveryMessage(OrderStatus $status): string
+{
+    return $status->value;
+}
+
+echo getDeliveryMessage(OrderStatus::Pending) . PHP_EOL;
+echo getDeliveryMessage(OrderStatus::Shipped) . PHP_EOL;
+echo getDeliveryMessage(OrderStatus::Delivered) . PHP_EOL;
