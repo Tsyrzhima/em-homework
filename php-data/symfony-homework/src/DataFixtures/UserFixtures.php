b4b1d798c1c638bcf3f3d921b7acd4cba52403d8
Tsyrzhima <sss1992bur@mail.ru>
2025-06-10 19:58:30 +0800

    Задание 6: миграции и сидирование в Doctrine

---
+++ php-data/symfony-homework/src/DataFixtures/UserFixtures.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 1; $i <= 5; $i++) {
+            $user = new User();
+            $user->setName("User $i");
+            $user->setEmail("user$i@example.com");
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
